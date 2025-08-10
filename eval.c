618f53e37b4f637a077377c72842fe6a64466a56
Michal Masrna <m.masrna1@gmail.com>
2025-08-10 15:39:39 +0200

    "final" eval.c + changed curve to SECP256K1

--- eval.c
+++ eval.c
@@ -1,11 +1,11 @@
 #include "include/sb_sw_lib.h"
 
 int main(void) {
-    sb_sw_private_t private_key;
-    sb_sw_public_t public_key;
-    sb_sw_context_t ct;
-    sb_sw_shared_secret_t secret;
-    // TODO: Decide on which curve to use
-    sb_sw_shared_secret(&ct, &secret, &private_key, &public_key, NULL, SB_SW_CURVE_P256, SB_DATA_ENDIAN_BIG);
-    return 0;
+  sb_sw_context_t ct;
+  sb_sw_public_t public_key;
+  sb_sw_private_t private_key;
+  sb_sw_shared_secret_t result;
+  sb_sw_shared_secret(&ct, &result, &private_key, &public_key, NULL,
+                      SB_SW_CURVE_SECP256K1, SB_DATA_ENDIAN_BIG);
+  return 0;
 }
